9a2eaed41f429702474ad01835d6c5defbddc3a8
Nguyen Tuan Kiet <nguyentuankiet23031998@gmail.com>
2017-03-24 14:12:43 +0700

    First Lession (1) v1.0

---
+++ LearningCPP/Main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+void main(void) {
+	printf("Hello World\n");
+	cout << "Hello 2st World" << endl;
+	int intA;
+	float floatB;
+	scanf("%d\n", &intA);
+	cin >> floatB;
+	cout << "Num A is " << intA << "\nNum B is " << floatB << endl;
+	getch();
+}
